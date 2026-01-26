0fbef6fe48cc252b4f48f2bb869b4afb1e8af97f
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2026-01-26 19:11:47 -0300

    Mapped DTO's to external API

---
+++ src/main/java/com/client/ws/rasmooplus/dto/wsraspay/PaymentDto.java
@@ -0,0 +1,17 @@
+package com.client.ws.rasmooplus.dto.wsraspay;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDto {
+
+    private CreditCardDto creditCard;
+
+    private String customerId;
+
+    private String orderId;
+}
