daeddb7bc61d596b81f0b8ad0eec36a9b324c253
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2026-04-09 13:11:14 -0300

    Use cacheEvict to detect database alterations

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -4,6 +4,7 @@ import com.client.ws.rasmooplus.dto.SubscriptionTypeDto;
 import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import jakarta.validation.Valid;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,16 +33,19 @@ public class SubscriptionTypeController {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findById(id));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @PostMapping()
     public ResponseEntity<SubscriptionType> createSubscriptionType(@Valid @RequestBody SubscriptionTypeDto subscriptionTypeDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(subscriptionTypeService.createSubscriptionType(subscriptionTypeDto));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @PutMapping("/{id}")
     public ResponseEntity<SubscriptionType> updateSubscriptionType(@PathVariable Long id, @RequestBody SubscriptionTypeDto subscriptionTypeDto) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.updateSubscriptionType(id, subscriptionTypeDto));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSubscriptionType(@PathVariable Long id) {
         subscriptionTypeService.deleteSubscriptionType(id);
